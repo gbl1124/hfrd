17ae386c6382f4860efd82c02323c9b3caed08c5
gbl1124 <gbl1124@163.com>
2020-04-10 18:35:11 +0800

    fix buf

--- backend/jenkins/pipelines/scripts/ibpv2/enroll_peer.sh
+++ backend/jenkins/pipelines/scripts/ibpv2/enroll_peer.sh
@@ -45,7 +45,7 @@ IFS=':' read -ra ADDR <<< "$CA_URL"
 export PROXY_IP=${ADDR[0]}
 RORG_CA_HOST=${ADDR[0]}
 RORG_CA_PORT=${ADDR[1]}
-NAME=${PEER_ORG_NAME}ca CA_HOST=${RORG_CA_HOST} CA_PORT=${!var1} ./wait_for_pod.sh
+NAME=${PEER_ORG_NAME}ca CA_HOST=${RORG_CA_HOST} CA_PORT=${RORG_CA_PORT} ./wait_for_pod.sh
 
 echo $TLS_CERT | base64 -d -w 0 > ${PEER_ORG_NAME_CA_TLS}/tls-ca-cert.pem
 
@@ -61,11 +61,11 @@ FABRIC_CLIENT_RC=$(($FABRIC_CLIENT_RC + $?))
 
 
 # Enroll MSP
-FABRIC_CA_CLIENT_HOME=${PEER_ORG_NAME_MSP} fabric-ca-client enroll -u https://peeradmin:pass4chain@${RORG_CA_HOST}:${RORG_CA_PORT} --caname ${CA_NAME}  --tls.certfiles ${PEER_ORG_NAME_CA_TLS}/tls-ca-cert.pem --csr.hosts ${CSRHOSTS}
+FABRIC_CA_CLIENT_HOME=${PEER_ORG_NAME_CA_ADMIN} fabric-ca-client enroll -u https://peeradmin:pass4chain@${RORG_CA_HOST}:${RORG_CA_PORT} --caname ${CA_NAME}   -M ${PEER_ORG_NAME_CA_ADMIN}/msp  --tls.certfiles ${PEER_ORG_NAME_CA_TLS}/tls-ca-cert.pem --csr.hosts ${CSRHOSTS}
 FABRIC_CLIENT_RC=$(($FABRIC_CLIENT_RC + $?))
 
 # register peer
-FABRIC_CA_CLIENT_HOME=${PEER_ORG_NAME_MSP} fabric-ca-client register -u https://admin:pass4chain@${RORG_CA_HOST}:${RORG_CA_PORT} --caname ${CA_NAME}  --id.name peer1 --id.secret pass4chain --id.type peer --tls.certfiles ${PEER_ORG_NAME_CA_TLS}/tls-ca-cert.pem --csr.hosts ${CSRHOSTS}
+FABRIC_CA_CLIENT_HOME=${PEER_ORG_NAME_CA_ADMIN} fabric-ca-client register -u https://admin:pass4chain@${RORG_CA_HOST}:${RORG_CA_PORT} --caname ${CA_NAME}  --id.name peer1 --id.secret pass4chain --id.type peer --tls.certfiles ${PEER_ORG_NAME_CA_TLS}/tls-ca-cert.pem --csr.hosts ${CSRHOSTS}
 FABRIC_CLIENT_RC=$(($FABRIC_CLIENT_RC + $?))
 
 #Enroll tlsca-admin with TLS CA
